a509f080e0521caad0232118786a7c861ef31e6d
emoron2553 <emoron@abatechnology.com>
2025-04-03 11:10:18 -0500

    Finished class winston part 2 - clas 43

--- src/app.js
+++ src/app.js
@@ -23,10 +23,20 @@
 // console.log(edward)
 
 // Siempre que hayan dependencias de tercero, adaptarlo con codigo mio
-const { getPokemonById } = require('./js-fundamentals/06-promises');
+// const { getPokemonById } = require('./js-fundamentals/06-promises');
 
-const info = getPokemonById(6).then(pokemon => console.log({pokemon})).catch(err => console.log(err));
+// const info = getPokemonById(6)
+//   .then((pokemon) => console.log({ pokemon }))
+//   .catch((err) => console.log(err))
+//   .finally(() => console.log('Finalmente'));
 
 // setTimeout(() => {
 //     console.log(info)
 // }, 3000)
+
+const { buildLogger } = require('./plugins');
+
+const logger = buildLogger('app.js ');
+
+logger.log('Hello world');
+logger.error('Esto es algo malo');
